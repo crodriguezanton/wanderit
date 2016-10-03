3f84fc46548bcd8a3ef6765e7257ae68f8d84aee
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-03 17:20:12 +0200

    django-jet
    Change admin interface to django-jet

--- wanderit/urls.py
+++ wanderit/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^jet/', include('jet.urls', 'jet')),
+    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     url(r'^admin/', admin.site.urls),
 ]
