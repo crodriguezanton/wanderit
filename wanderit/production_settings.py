7e19dd08c884af41e2f8253e256ec49f4aad2d45
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-03 15:50:12 +0200

    Production settings

--- wanderit/production_settings.py
+++ wanderit/production_settings.py
@@ -13,3 +13,14 @@ DATABASES = {
         'PASSWORD': 'beatbcn4113',
     }
 }
+
+
+""" Static files and media (CSS, JavaScript, Images) """
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "../static")]
+ENV_PATH = os.path.abspath(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(ENV_PATH, '../../static_server/media/')
+STATIC_ROOT = os.path.join(ENV_PATH, '../../static_server/static/')
+
+# This one is just useful during development, on production statics and media should be served by nginx / apache
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
