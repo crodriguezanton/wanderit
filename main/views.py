1c42c4a66953b0ccb757df850853e10edb145eb9
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-09 01:32:53 +0200

    Dynamic start

--- main/views.py
+++ main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from searchreports.models import Report
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'main/home.html'
+    model = Report
+
+    def get_context_data(self, **kwargs):
+
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        return context
