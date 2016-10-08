bc063d18e160be0fa96f122907866e173b608a4c
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-08 16:47:28 +0200

    Fix

--- searchreports/admin.py
+++ searchreports/admin.py
@@ -3,9 +3,9 @@ from models import *
 
 
 class ReportUpdateAdmin(admin.ModelAdmin):
-    fields = ('search_report__flight_search__created', 'search_report__min_price', 'abs_diff',
-              'abs_deriv', 'has_min_extremum', 'has_positive_change_of_concavity', 'has_negative_change_of_concavity')
-    list_filter = ('search_report__report')
+    fields = ['search_report__flight_search__created', 'search_report__min_price', 'abs_diff',
+              'abs_deriv', 'has_min_extremum', 'has_positive_change_of_concavity', 'has_negative_change_of_concavity']
+    list_filter = ['search_report__report']
 
 admin.site.register(Report)
 admin.site.register(SearchReport)
