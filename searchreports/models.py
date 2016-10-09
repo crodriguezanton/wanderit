44be4f1d78d8c3c3298a0e7d6257c3f79e983a74
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-09 02:58:53 +0200

    Fix

--- searchreports/models.py
+++ searchreports/models.py
@@ -32,7 +32,8 @@ class Report(models.Model):
         return self.searchreport_set.all().order_by('-flight_search__created').first().min_price_itinerary.pricingoption_set.all().order_by('price').first()
 
     def get_price_trend(self):
-        reports = self.searchreport_set.all().order_by('-flight_search__created')[:2]
+        dummyreports = self.searchreport_set.all().order_by('-flight_search__created').values('pk')[:2]
+        reports = SearchReport.objects.filter(pk__in=dummyreports)
         if reports.filter(reportupdate__has_negative_change_of_concavity=True).count():
             return 'UP'
         if reports.filter(reportupdate__has_min_extremum=True).count():
