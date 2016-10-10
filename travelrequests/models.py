f90723ef74b51f9ed6da919825b9b96db0b04684
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-11 00:42:05 +0200

    New models

--- travelrequests/models.py
+++ travelrequests/models.py
@@ -1,9 +1,9 @@
-from __future__ import unicode_literals
-
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
+from cron.models import FlightSearchCronJob
 from main.models import WanderitUser
+from searchreports.models import Report
 from skyscannerSDK.models import Place
 
 
@@ -21,6 +21,17 @@ class UserDatesRequest(TimeStampedModel):
     max_nights = models.IntegerField()
 
 
+class SearchRequest(models.Model):
+    origin = models.ForeignKey(Place, related_name='origin_city_request')
+    destination = models.ForeignKey(Place, related_name='destination_city_request')
+    outbound = models.DateField()
+    inbound = models.DateField()
+    report = models.OneToOneField(Report, null=True)
+    cron = models.OneToOneField(FlightSearchCronJob)
+
+
 class UserRequestMatch(models.Model):
+    wiuser = models.ForeignKey(WanderitUser, null=True)
     destination_request = models.ForeignKey(UserDestinationRequest)
     dates_request = models.ForeignKey(UserDatesRequest)
+    search_request = models.ForeignKey(SearchRequest, null=True)
