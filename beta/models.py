50889989af282ebd3a6373c38dc66475460288e2
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-11 13:17:49 +0200

    Beta codes

---
+++ beta/models.py
@@ -0,0 +1,8 @@
+import uuid
+
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+
+class BetaCode(TimeStampedModel):
+    code = models.UUIDField(default=uuid.uuid4, primary_key=True)
