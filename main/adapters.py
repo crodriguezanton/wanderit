cbc2ff3e8bcd7e5afb8ade864120cb140e8b7ddb
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-13 21:19:05 +0200

    Boarding pass changes

--- main/adapters.py
+++ main/adapters.py
@@ -1,16 +1,28 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
+from main.models import WanderitUser
 from wanderit import settings
 
 
 class AccountAdapter(DefaultAccountAdapter):
 
+    def save_user(self, request, user, form, commit=True):
+        user = super(AccountAdapter, self).save_user(request, user, form, commit=commit)
+        wiuser = WanderitUser.objects.create(user=user)
+
+
+    @staticmethod
     def is_open_for_signup(self, request):
         return settings.OPEN_FOR_SIGNUP
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
 
+    @staticmethod
     def is_open_for_signup(self, request, sociallogin):
         return settings.OPEN_FOR_SIGNUP
+
+    def save_user(self, request, sociallogin, form=None):
+        user = super(SocialAccountAdapter, self).save_user(request, sociallogin, form=form)
+        wiuser = WanderitUser.objects.create(user=user)
