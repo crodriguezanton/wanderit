ac58c39f43a83875b0f21915285083e6f37cd7e9
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-17 18:49:16 +0200

    Raven

--- wanderit/production_settings.py
+++ wanderit/production_settings.py
@@ -36,3 +36,51 @@ STATIC_ROOT = os.path.join(ENV_PATH, '../static_server/static/')
 # This one is just useful during development, on production statics and media should be served by nginx / apache
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+
+RAVEN_CONFIG = {
+    'dsn': 'https://9cf7753c476b4127b02d337e78781a86:95efbc9a16354e5e863e7c2b43ebcc50@sentry.io/106800',
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'WARNING', # To capture more than ERROR, change to WARNING, INFO, etc.
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'root': {
+            'level': 'WARNING',
+            'handlers': ['sentry'],
+        },
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
